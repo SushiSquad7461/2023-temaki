17756fcffea326ddc429a62f4b5e611d06c396b0
Daniel Gorbatov <dzgorbatov@gmail.com>
2023-01-11 08:55:49 -0800

    chescktyle fixes

--- src/main/java/frc/robot/util/SwerveModuleConstants.java
+++ src/main/java/frc/robot/util/SwerveModuleConstants.java
@@ -1,22 +1,23 @@
 package frc.robot.util;
 
+/**
+ * Wrapper class for swerve module constants.
+ */
 public class SwerveModuleConstants {
-    public final int driveMotorID;
-    public final int angleMotorID;
-    public final int cancoderID;
+    public final int driveMotorId;
+    public final int angleMotorId;
+    public final int cancoderId;
     public final double angleOffset;
 
     /**
      * Swerve Module Constants to be used when creating swerve modules.
-     * @param driveMotorID
-     * @param angleMotorID
-     * @param canCoderID
-     * @param angleOffset
      */
-    public SwerveModuleConstants(int driveMotorID, int angleMotorID, int canCoderID, double angleOffset) {
-        this.driveMotorID = driveMotorID;
-        this.angleMotorID = angleMotorID;
-        this.cancoderID = canCoderID;
+    public SwerveModuleConstants(
+        int driveMotorId, int angleMotorId, int canCoderId, double angleOffset
+    ) {
+        this.driveMotorId = driveMotorId;
+        this.angleMotorId = angleMotorId;
+        this.cancoderId = canCoderId;
         this.angleOffset = angleOffset;
     }
 }
