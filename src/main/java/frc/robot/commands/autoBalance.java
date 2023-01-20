3a70769231eaba7303cd087f3c65cb9210a0378c
Maanav-s <92064736+Maanav-s@users.noreply.github.com>
2023-01-19 17:05:14 -0800

    wheel cross untested

--- src/main/java/frc/robot/commands/autoBalance.java
+++ src/main/java/frc/robot/commands/autoBalance.java
@@ -43,7 +43,7 @@ public class autoBalance extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    //TODO: cross wheels
+    swerve.drive(new Translation2d(0, 0), 0.01, true, false);
   }
 
   // Returns true when the command should end.
