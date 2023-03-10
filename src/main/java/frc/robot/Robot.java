b8b859b0030872fe4b5ac479304565dfdd7610c8
Daniel Gorbatov <dzgorbatov@gmail.com>
2023-03-09 18:56:34 -0800

    added compressor (that doesn't work yet)

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,10 +4,13 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.subsystems.Swerve;
@@ -27,6 +30,7 @@ public class Robot extends TimedRobot {
     private Arm arm;
     private RobotContainer robotContainer;
     private Swerve swerve;
+    private Compressor compressor;
 
     /**
      * This function is run when the robot is first started up and should be used for any
@@ -46,6 +50,7 @@ public class Robot extends TimedRobot {
         }
 
         swerve = Swerve.getInstance();
+        compressor = new Compressor(1, PneumaticsModuleType.REVPH);
 
         DataLogManager.start();
         DriverStation.startDataLog(DataLogManager.getLog());
@@ -61,6 +66,7 @@ public class Robot extends TimedRobot {
     @Override
     public void robotPeriodic() {
         CommandScheduler.getInstance().run();
+        SmartDashboard.putNumber("compressor pressure", compressor.getPressure());
     }
 
     @Override
