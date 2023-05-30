7899ae465efe4c1bc3dab51ba8d89fb4443f77e2
Daniel Gorbatov <dzgorbatov@gmail.com>
2023-05-30 07:56:11 -0700

    util cleanup

--- src/main/java/frc/robot/util/LED.java
+++ src/main/java/frc/robot/util/LED.java
@@ -2,15 +2,22 @@ package frc.robot.util;
 
 import edu.wpi.first.wpilibj.PWM;
 
+/**
+ * Controls a PWM Led strip.
+ */
 public class LED {
     PWM led;
+
     public LED(int port) {
         led = new PWM(port);
     }
 
+    /**
+     * Sets the value of the led strip.
+     */
     public void set(double value, double pulseWidth) {
-        pulseWidth/=1000.0;
-        led.setBounds(2.005, value+0.005, value, value-0.005, 0.995);
+        pulseWidth /= 1000.0;
+        led.setBounds(2.005, value + 0.005, value, value - 0.005, 0.995);
         led.setPeriodMultiplier(PWM.PeriodMultiplier.k1X);
         led.setSpeed(value);
         led.setZeroLatch();
--- src/main/java/frc/robot/util/VisionMeasurement.java
+++ src/main/java/frc/robot/util/VisionMeasurement.java
@@ -1,16 +1,26 @@
 package frc.robot.util;
 
+import edu.wpi.first.math.geometry.Pose2d;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
-import edu.wpi.first.math.geometry.Pose2d;
-
+/**
+ * Class to hold vision measurments.
+ */
 public class VisionMeasurement {
     public final PhotonTrackedTarget target;
     public final Pose2d robotPose;
     public final double timestampSeconds;
     public final double ambiguity;
-    
-    public VisionMeasurement(PhotonTrackedTarget target, Pose2d robotPose, double timestampSeconds, double ambiguity) {
+   
+    /**
+     * Construct class.
+     */
+    public VisionMeasurement(
+        PhotonTrackedTarget target, 
+        Pose2d robotPose, 
+        double timestampSeconds, 
+        double ambiguity
+    ) {
         this.target = target;
         this.robotPose = robotPose;
         this.timestampSeconds = timestampSeconds;
